3ad23b4d301c7cfd9b52643126ce87d2395aa8a4
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-05-27 12:14:26 +0200

    Fine impostazione dell'Header, inizio stilizzazione del JumboCard Component

--- proj-html-vuejs/src/components/HeaderComp.vue
+++ proj-html-vuejs/src/components/HeaderComp.vue
@@ -1,15 +1,67 @@
 <template>
 <header>
-HEADER
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-between align-items-center">
+                <div class="logo">
+                    <img src="../assets/images/white-logo-2.png" alt="PhloxCorpLogo">
+                </div>
+                <nav class="d-flex">
+                    <a v-for="(el, i) in navLinksArray" :key="`NavLinks${i}`" href="#">{{el}}</a>
+                    <div class="my-btn">QUI CI METTO UN BTNCOMP</div>
+                </nav>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 d-flex align-items-center">
+                <JumboCard />
+            </div>
+            <div class="col-6">
+                <img src="../assets/images/header.png" alt="header.png">
+            </div>
+        </div>
+    </div>
 </header>
 </template>
 
 <script>
-export default {
+import JumboCard from "./utils_comp/JumboCard.vue";
 
+export default {
+    components: { JumboCard },
+    data() {
+        return {
+            navLinksArray: [
+                "Home",
+                "About",
+                "Service",
+                "Blog",
+                "Contact",
+                "Portfolio",
+            ]
+        };
+    },
 }
 </script>
 
 <style scoped lang="scss">
+@import '../assets/style/vars';
+
+header{
+    background: linear-gradient($bg-blue, $bg-dark-4);
+    .logo{
+        padding: 10px;
+    }
+    a{
+        color: white;
+        margin: 0 .5rem;
+    }
+    .my-btn{
+        margin: 0 2rem;
+    }
+    .col-6 img{
+        width: 100%;
+    }
+}
 
 </style>
---
+++ proj-html-vuejs/src/components/utils_comp/JumboCard.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="box">
+    <div class="titles">
+        <h6 class="pretext">Io qui non ci sono</h6>
+        <h1 class="bold">HeyHey</h1>
+        <h1 class="thin">HeyHeyHey</h1>
+        <h1 class="bold">Hey</h1>
+    </div>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit fuga iure sapiente delectus illum optio cumque dolor itaque labore, tempora aperiam non.</p>
+    <div class="btn-container">
+        <span>BTNCOMP</span>
+        <span>BTNCOMP</span>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+.box{
+    padding: 1vw;
+    .titles{
+        color: white;
+        h1.bold{
+            font-weight: bolder;
+        }
+    }
+}
+</style>
